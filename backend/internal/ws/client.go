3be6f4d958daea50907f7b61ccf3d4171eadb34d
rainz <ronakkamboj26@gmail.com>
2026-07-22 22:55:26 +0530

    feat(backend): add websocket hub and client pumps

--- backend/internal/ws/client.go
+++ backend/internal/ws/client.go
@@ -1,1 +1,121 @@
 package ws
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"songspot/internal/models"
+	"time"
+
+	"github.com/gorilla/websocket"
+)
+
+const (
+	writeWait  = 10 * time.Second
+	pongWait   = 60 * time.Second
+	pingPeriod = (pongWait * 9) / 10
+)
+
+// middleman between the websocket connection and the hub
+type Client struct {
+	Hub    *Hub
+	UserID string
+	Conn   *websocket.Conn
+	Send   chan []byte
+}
+
+// readpump pumps messages from the websocket connection to the hub this runs in a dedicated goroutine for each client
+func (c *Client) ReadPump() {
+	defer func() {
+		c.Hub.Unregister <- c
+		c.Conn.Close()
+	}()
+
+	c.Conn.SetReadDeadline(time.Now().Add(pongWait))
+	c.Conn.SetPongHandler(func(string) error {
+		c.Conn.SetReadDeadline(time.Now().Add(pongWait))
+		return nil
+	})
+
+	for {
+		_, message, err := c.Conn.ReadMessage()
+		if err != nil {
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+				log.Printf("error:%v", err)
+			}
+			break
+		}
+
+		var event models.WSEvent
+		if err = json.Unmarshal(message, &event); err == nil && event.Action == "ping" {
+			dataMap, ok := event.Data.(map[string]interface{})
+			if ok {
+				clientTimeValue, ok := dataMap["clientTime"].(float64)
+				if !ok {
+					continue
+				}
+
+				pongData := models.TimeSyncData{
+					ClientTime: int64(clientTimeValue),
+					ServerTime: time.Now().UnixMilli(),
+				}
+
+				pongEvent := models.WSEvent{
+					Action:    "pong",
+					Data:      pongData,
+					Timestamp: time.Now().UnixMilli(),
+				}
+
+				if responseMsg, err := json.Marshal(pongEvent); err == nil {
+					c.Send <- responseMsg // bound directly back to this client
+				}
+			}
+			continue
+		}
+
+		// c.Hub.Broadcast <- message, we dont do this anymore instead of directly publish to redisClie        //so that all the servers hear it
+		ctx := context.Background()
+		channelName := "room_events:" + c.Hub.RoomID
+		err = c.Hub.redisClient.Publish(ctx, channelName, string(message)).Err()
+		if err != nil {
+			log.Printf("Failed to publish to Redis: %v", err)
+		}
+	}
+}
+
+// writepump pumps messages from the hub to the websocket connection
+// also runs in a dedicated goroutine for each client
+func (c *Client) WritePump() {
+	ticker := time.NewTicker(pingPeriod)
+	defer func() {
+		ticker.Stop()
+		c.Conn.Close()
+	}()
+
+	for {
+		select {
+		case message, ok := <-c.Send:
+			c.Conn.SetWriteDeadline(time.Now().Add(writeWait))
+			if !ok {
+				c.Conn.WriteMessage(websocket.CloseMessage, []byte{})
+				return
+			}
+
+			w, err := c.Conn.NextWriter(websocket.TextMessage)
+			if err != nil {
+				return
+			}
+
+			w.Write(message)
+
+			if err := w.Close(); err != nil {
+				return
+			}
+		case <-ticker.C:
+			c.Conn.SetWriteDeadline(time.Now().Add(writeWait))
+			if err := c.Conn.WriteMessage(websocket.PingMessage, nil); err != nil {
+				return
+			}
+		}
+	}
+}
--- backend/internal/ws/hub.go
+++ backend/internal/ws/hub.go
@@ -1,1 +1,76 @@
 package ws
+
+import (
+	"context"
+	"log"
+
+	"github.com/redis/go-redis/v9"
+)
+
+type Hub struct {
+	RoomID      string
+	Clients     map[*Client]bool
+	Broadcast   chan []byte
+	Register    chan *Client
+	Unregister  chan *Client
+	redisClient *redis.Client
+	pubSub      *redis.PubSub
+}
+
+func NewHub(roomID string, rdb *redis.Client) *Hub {
+	return &Hub{
+		RoomID:      roomID,
+		Broadcast:   make(chan []byte),
+		Register:    make(chan *Client),
+		Unregister:  make(chan *Client),
+		Clients:     make(map[*Client]bool),
+		redisClient: rdb,
+	}
+}
+
+// starts the hub's event loop
+// should be called as goroutine: go hub.Run()
+func (h *Hub) Run() {
+	ctx := context.Background()
+	channelName := "room_events:" + h.RoomID
+	h.pubSub = h.redisClient.Subscribe(ctx, channelName)
+
+	go h.listenToRedis()
+	defer func() {
+		h.pubSub.Close()
+	}()
+	for {
+		select {
+		case client := <-h.Register:
+			h.Clients[client] = true
+
+		case client := <-h.Unregister:
+			if _, ok := h.Clients[client]; ok {
+				delete(h.Clients, client)
+				close(client.Send)
+			}
+
+		case message := <-h.Broadcast:
+			for client := range h.Clients {
+				select {
+				case client.Send <- message:
+					// message queued successfully
+				default:
+					// if the client's send channel buffer is full, they are
+					// dead or too slow. Drop them to save memory
+					close(client.Send)
+					delete(h.Clients, client)
+				}
+			}
+		}
+	}
+}
+
+func (h *Hub) listenToRedis() {
+	ch := h.pubSub.Channel()
+
+	for msg := range ch {
+		h.Broadcast <- []byte(msg.Payload)
+	}
+	log.Printf("Redis Pub/Sub channel closed for room %s", h.RoomID)
+}
